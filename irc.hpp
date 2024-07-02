8c95e8d68cbc6d2e5bddc6d28cb4e719ed31be9a
matde-je <matidlecosta719@gmail.com>
2024-07-02 16:29:47 +0100

    ok

--- irc.hpp
+++ irc.hpp
@@ -8,3 +8,25 @@
 #include <algorithm>
 #include <cstdlib>
 
+#include <vector> 
+#include <sys/socket.h> //-> for socket()
+#include <sys/types.h> //-> for socket()
+#include <netinet/in.h> //-> for sockaddr_in
+#include <unistd.h> //-> for close()
+#include <arpa/inet.h> //-> for inet_ntoa()
+#include <poll.h> //-> for poll()
+#include <csignal> //-> for signal()
+
+class Client {
+    private :
+
+    public :
+
+};
+
+class Server {
+    private :
+
+    public :
+
+};
--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@ int main(int argc, char **argv) {
     if (argc != 3) {std::cerr << "Invalid number of arguments.\n"; return 1;}
     char* endptr;
     long num = strtol(argv[1], &endptr, 10);
-    if (endptr == argv[1]) 
-        {std::cerr << "Error: invalid port number."; return 1;}
-    if (num > 65535 || num <= 0 || argv[1][0] == '0') {std::cerr << "Error: invalid port.\n"; return 1;}
+    if (endptr == argv[1]|| num > 65535 || num <= 0 || argv[1][0] == '0') {std::cerr << "Error: invalid port.\n"; return 1;}
   
 
 }
